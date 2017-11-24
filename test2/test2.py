2053a321f6dcaf96481709cf8df7d38c07e3fc08
NotHavoc <33915989+NotHavoc@users.noreply.github.com>
2017-11-24 13:47:56 -0500

    Update test2.py

--- test2/test2.py
+++ test2/test2.py
@@ -12,16 +12,41 @@ from random import choice
 
 class Test2:
     """My custom cog that does stuff!"""
+    triggered
 
     def __init__(self, bot):
         self.bot = bot
+        self.triggered = false
+         
 
     @commands.command()
-    async def mycom(self):
+    async def test(self):
         """This does stuff!"""
         str = "I can do stuff
         #Your code will go here
-        await self.bot.say("I can do stuff!")
+        await self.bot.say(str)
+        
+    async def on_message(self, message):
+        channel = message.channel
+        author = message.author
+
+        if message.server is None:
+            return
+
+        if author == self.bot.user:
+            return
+
+        if not self.bot.user_allowed(message):
+            return
+        
+        day = datetime.datetime.now().isoweekday()
+        
+        if day != 3
+            self.triggered = false
+            return
+        
+        self.triggered = true
+        await self.bot.say("It is Wednesday my dudes.")    
 
 def setup(bot):
     bot.add_cog(Test2(bot))
